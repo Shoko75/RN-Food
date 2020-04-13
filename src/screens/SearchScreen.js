fc695fb761291a3deb9a0eb50210490187b391a3
kosho75 <kosho75@gmail.com>
2020-04-13 11:43:13 -0700

    Implemented Restaurants Search Page

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,22 +1,34 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useRestaurants from '../hooks/useRestaurants';
+import RestaurantsList from '../components/RestaurantsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, restaurants, errorMessage] = useRestaurants();
 
+  const filterRestaurantsByPrice = (price) => {
+    // price === '$' || '$$' || '$$$'
+    return restaurants.filter(restaurant => {
+      return restaurant.price === price;
+    });
+  };
+
   return (
-    <View>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {restaurants.length} results</Text>
-    </View>
+      <ScrollView>
+        <RestaurantsList restaurants={filterRestaurantsByPrice('$')} title="Cost Effective" />
+        <RestaurantsList restaurants={filterRestaurantsByPrice('$$')} title="Bit Pricier" />
+        <RestaurantsList restaurants={filterRestaurantsByPrice('$$$')} title="Big Spender" />
+      </ScrollView>
+    </>
   );
 };
 
